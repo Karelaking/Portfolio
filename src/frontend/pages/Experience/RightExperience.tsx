3ca02cccc51078cedd157e85a66daee9ef29384b
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 15:48:33 +0530

    Experience, LeftExperience, RightExperience Completed

---
+++ src/frontend/pages/Experience/RightExperience.tsx
@@ -0,0 +1,7 @@
+import { screenSizeConstants } from "../../constants/SizeConstants";
+
+const RightExperience = () => {
+  return <div className={`${screenSizeConstants.subScreenSize}`}>RightExperience</div>;
+}
+
+export default RightExperience;
