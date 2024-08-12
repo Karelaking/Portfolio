5b1061d8837e4fc5cfaf8b95d38a9784f4347305
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 11:03:54 +0530

    Navigation Bar Completed

--- src/frontend/components/Logo/Logo.tsx
+++ src/frontend/components/Logo/Logo.tsx
@@ -3,17 +3,17 @@ import { logoConstants } from "../../constants/StringConstants";
 import { colorConstants } from "../../constants/ColorsConstants";
 
 
-const outerLogoCssStyle = `${colorConstants.primatyTextColor} font-lalezar uppercase text-3xl`;
-const innerLogoCssStyle = `${colorConstants.activeTextColor} lg:mx-2`;
+const outerLogoCssStyle = `${colorConstants.primatyTextColor} font-lalezar p-0 m-0 uppercase flex justify-center lg:text-3xl text-xl`;
+const innerLogoCssStyle = `${colorConstants.activeTextColor} lg:mx-2 p-0 m-0 flex justify-center`;
 
 const Logo = () => {
-  return <Link to="/" key="/">
-    <span className={`${outerLogoCssStyle}`}>
+  return <Link className={`flex flex-1`} to="/" key="/">
+    <div className={`${outerLogoCssStyle}`}>
         {logoConstants.firstPart}
-        <span className={`${innerLogoCssStyle}`}>
+        <div className={`${innerLogoCssStyle}`}>
             {logoConstants.lastPart}
-        </span>
-    </span>
+        </div>
+    </div>
   </Link>;
 }
 
