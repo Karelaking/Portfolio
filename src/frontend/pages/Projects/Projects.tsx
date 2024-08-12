d15ec6d750c829bf079525028f3e41b382c0cce2
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 15:42:53 +0530

    Projects, LeftProjects, RightProjects Completed

--- src/frontend/pages/Projects/Projects.tsx
+++ src/frontend/pages/Projects/Projects.tsx
@@ -1,7 +1,12 @@
 import { screenSizeConstants } from "../../constants/SizeConstants";
+import LeftProjects from "./LeftProjects";
+import RightProjects from "./RightProjects";
 
 const Projects = () => {
-  return <section className={`${screenSizeConstants.mainScreenSize}`}>this is projects section</section>;
+  return <section className={`${screenSizeConstants.mainScreenSize}`}>
+    <LeftProjects/>
+    <RightProjects/>
+  </section>;
 }
 
 export default Projects
