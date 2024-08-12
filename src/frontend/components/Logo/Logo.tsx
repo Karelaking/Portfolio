1050748d21aaae4a74478bc16a36ed31725f45b5
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 21:57:45 +0530

    New Style To Logo

--- src/frontend/components/Logo/Logo.tsx
+++ src/frontend/components/Logo/Logo.tsx
@@ -4,10 +4,10 @@ import { colorConstants } from "../../constants/ColorsConstants";
 
 
 const outerLogoCssStyle = `${colorConstants.primatyTextColor} font-lalezar p-0 m-0 uppercase flex justify-center lg:text-3xl text-xl`;
-const innerLogoCssStyle = `${colorConstants.activeTextColor} lg:mx-2 p-0 m-0 flex justify-center`;
+const innerLogoCssStyle = `${colorConstants.activeTextColor} ${colorConstants.activeBorderColor} border-b-2 lg:mx-2 p-0 m-0 flex justify-center`;
 
 const Logo = () => {
-  return <Link className={`flex flex-1`} to="/" key="/">
+  return <Link className={`flex`} to="/" key="/">
     <div className={`${outerLogoCssStyle}`}>
         {logoConstants.firstPart}
         <div className={`${innerLogoCssStyle}`}>
