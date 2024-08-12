555aef894da163e902ce114674af965e8503faa2
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 14:09:14 +0530

    Left and Right Home Completed

---
+++ src/frontend/pages/Home/LeftHome.tsx
@@ -0,0 +1,39 @@
+import { colorConstants } from "../../constants/ColorsConstants";
+import { screenSizeConstants } from "../../constants/SizeConstants";
+import { homePageConstants } from "../../constants/StringConstants";
+
+const LeftHome = () => {
+  return (
+    <div
+      className={`${screenSizeConstants.subScreenSize} flex-col`}
+    >
+      <div className={`${colorConstants.activeBorderColor} border-l-4 px-4`}>
+        <div
+          className={`${colorConstants.primatyTextColor} font-comfortaa text-md lg:text-xl`}
+        >
+          {homePageConstants.welcomeText}
+        </div>
+        <div
+          className={`${colorConstants.primatyTextColor} flex font-lalezar uppercase lg:text-7xl`}
+        >
+          {homePageConstants.firstPart}
+          <div className={`${colorConstants.activeTextColor} mx-3`}>
+            {homePageConstants.secondPart}
+          </div>
+        </div>
+        <div
+          className={`${colorConstants.primatyTextColor} font-comfortaa text-md lg:text-xl`}
+        >
+          {homePageConstants.welcomeText2}
+        </div>
+        <div
+          className={`${colorConstants.primatyTextColor} font-comfortaa text-md lg:text-xl`}
+        >
+          {homePageConstants.mainText}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftHome;
---
+++ src/frontend/pages/Home/RightHome.tsx
@@ -0,0 +1,9 @@
+import { screenSizeConstants } from "../../constants/SizeConstants"
+
+const RightHome = () => {
+  return (
+    <div className={`${screenSizeConstants.subScreenSize} flex-col`}>RightHome</div>
+  )
+}
+
+export default RightHome
