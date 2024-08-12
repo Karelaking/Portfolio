06e806e5e2d0221cecdae1ad1636f13f86201887
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 15:25:19 +0530

    About, LeftAbout, RightAbout Completed

---
+++ src/frontend/pages/About/LeftAbout.tsx
@@ -0,0 +1,7 @@
+import { screenSizeConstants } from "../../constants/SizeConstants";
+
+const LeftAbout = () =>  {
+  return <div className={`${screenSizeConstants.subScreenSize}`}>LeftAbout</div>;
+}
+
+export default LeftAbout
