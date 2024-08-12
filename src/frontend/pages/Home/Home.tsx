950bb1e1781bd068cd518a6a499fdd071fb8b8f3
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 13:37:38 +0530

    Home Completed

--- src/frontend/pages/Home/Home.tsx
+++ src/frontend/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
 import {homePageConstants} from '../../constants/StringConstants';
 import { screenSizeConstants } from '../../constants/SizeConstants';
+import LeftHome from './LeftHome';
+import RightHome from './RightHome';
 
 const Home = () => {
-  return <section className={`${screenSizeConstants.mainScreenSize}`}>{homePageConstants.heroTitle}</section>;
+  return <section className={`${screenSizeConstants.mainScreenSize}`}>
+    <LeftHome/>
+    <RightHome/>
+  </section>;
 }
 
 export default Home
