cea589cd43491dc995d6daf5331127ff6cd1b9c6
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-12 15:33:02 +0530

    Skills, LeftSkill, RightSkill Completed

---
+++ src/frontend/pages/Skills/RightSkills.tsx
@@ -0,0 +1,7 @@
+import { screenSizeConstants } from "../../constants/SizeConstants";
+
+const RightSkills = () => {
+  return <div className={`${screenSizeConstants.subScreenSize}`}>RightSkills</div>;
+}
+
+export default RightSkills;
