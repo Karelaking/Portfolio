6fef3636ce3446bacbeaaac9e798ef781b68520a
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-13 12:13:51 +0530

    Minor Fixses

--- src/frontend/components/Maps/ContactsMap.tsx
+++ src/frontend/components/Maps/ContactsMap.tsx
@@ -1,7 +1,7 @@
 import { colorConstants } from "../../constants/ColorsConstants";
 
 const ContactsMap = () => {
-  return <div className={`w-full rounded-md`}>
+  return <div className={`w-full my-10 lg:my-0 rounded-md`}>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d228625.17205286093!2d80.17323066304016!3d26.447078786158215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399c4770b127c46f%3A0x1778302a9fbe7b41!2sKanpur%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1723488451008!5m2!1sen!2sin" className={`w-full lg:h-[24rem] ${colorConstants.primaryBackgroundColor} mb-10 rounded-md`} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
   </div>;
 }
--- src/frontend/components/MediaIcons/MediaIcons.tsx
+++ src/frontend/components/MediaIcons/MediaIcons.tsx
@@ -7,7 +7,7 @@ type MediaIconsType = {
 };
 
 const MediaIcons = ({link, svgIcon}: MediaIconsType) => {
-  return <div className={`${colorConstants.secondaryBackgroundColor} w-max h-max p-4 rounded-full opacity-70 ${colorConstants.primaryBorderColor} border-2 backdrop-blur-sm hover:scale-125 duration-500`}>
+  return <div className={`${colorConstants.secondaryBackgroundColor} w-max h-max lg:p-4 p-3 flex-wrap rounded-full opacity-70 ${colorConstants.primaryBorderColor} border-2 backdrop-blur-sm hover:scale-125 duration-500`}>
     <Link to={link}>
       {svgIcon}
     </Link>
