36d3af36834a98b9d3a27e3c4021f64d1b3bbea3
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-13 12:13:51 +0530

    Minor Fixses

--- src/frontend/components/MediaIcons/MediaBar.tsx
+++ src/frontend/components/MediaIcons/MediaBar.tsx
@@ -2,10 +2,10 @@ import MediaIcons from "./MediaIcons";
 import { mediaIconsPathConstants } from "../../constants/PathConstants";
 
 const MediaBar = () => {
-  return <div className="w-full h-max my-16 gap-4 items-center flex">
+  return <div className="w-full h-max lg:my-16 my-8 lg:gap-4 gap-3 items-center flex">
     {mediaIconsPathConstants.map((mediaIconsPathConstant) =>{
         return <MediaIcons link={mediaIconsPathConstant.path} svgIcon={mediaIconsPathConstant.svg}/>
-    })};
+    })}
   </div>;
 }
 
