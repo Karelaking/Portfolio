02ce86ad8859dc8a3c92c32551c85986028e9dcf
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-13 11:58:47 +0530

    Optimaise for Mobile

---
+++ src/frontend/components/Headings/PageHeading.tsx
@@ -0,0 +1,22 @@
+import { colorConstants } from "../../constants/ColorsConstants";
+
+type PageHeadingType = {
+  firstTitle: string;
+  lastTitle: string;
+};
+
+const PageHeading = (props: PageHeadingType) => {
+  return (
+    <div
+      className={`${colorConstants.primatyTextColor} flex capitalize font-korona_one lg:text-4xl text-3xl lg:my-4`}
+    >
+      {props.firstTitle}
+      <div
+        className={`${colorConstants.activeTextColor} mx-2 lg:mx-3 border-b-4 ${colorConstants.activeBorderColor}`}
+      >
+        {props.lastTitle}
+      </div>
+    </div>
+  );
+};
+export default PageHeading;
