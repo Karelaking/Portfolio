cab91e02a1f0063a1ad317dbf5384cd3f624ed6d
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-14 22:26:16 +0530

    Added Model in RightHome

---
+++ src/frontend/components/CanvasLoader/CanvasLoader.tsx
@@ -0,0 +1,31 @@
+import { Html, useProgress } from "@react-three/drei";
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html
+    //   as='Canvas'
+      center
+      style={{
+        display: "flex",
+        // justifyContent: "center",
+        // alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          fontWeight: 800,
+          marginTop: 40,
+          color:"GrayText"
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
