0cca341237ca2a75b10887b0418aca32d8fd3e82
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-23 07:35:22 +0530

    feat: Add Components.tsx to export components

---
+++ src/frontend/components/Components.tsx
@@ -0,0 +1,27 @@
+import CanvasLoader from "./CanvasLoader/CanvasLoader";
+import SkillaCard from "./Cards/SkillsCard";
+import Clipboard from "./Clipboard/Clipboard";
+import Cookies1 from "./Cookies/Cookies";
+import Footer from "./Footer/Footer";
+import PageHeading from "./Headings/PageHeading";
+import Logo from "./Logo/Logo";
+import ContactsMap from "./Maps/ContactsMap";
+import MediaIcons from "./MediaIcons/MediaIcons";
+import Navigation from "./NavigationBar/Navigation";
+import ProgressBar from "./ProgressBar/ProgressBar";
+import Video from "./Videos/Videos";
+
+export {
+  CanvasLoader,
+  SkillaCard,
+  Clipboard,
+  Cookies1,
+  Footer,
+  PageHeading,
+  Logo,
+  ContactsMap,
+  MediaIcons,
+  Navigation,
+  ProgressBar,
+  Video,
+};
