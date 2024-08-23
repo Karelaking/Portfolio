38b4ea6f39622af7107e83ce9d9486f05b78a3de
mradul kumar katiyar <karelaking277@gmai.com>
2024-08-23 07:30:12 +0530

    feat: Add Pages.tsx to export frontend pages

---
+++ src/frontend/pages/Pages.tsx
@@ -0,0 +1,25 @@
+import Root from "./Root/Root";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
+import Experience from "./Experience/Experience";
+import Signin from "./SignIn/SignIn";
+import Contacts from "./Contacts/Contacts";
+import Admin from "./Admin/Admin";
+import ErrorPage from "./Error/Error";
+import Splash from "./Splash/Splash";
+
+export {
+  Root,
+  Home,
+  About,
+  Skills,
+  Projects,
+  Experience,
+  Signin,
+  Contacts,
+  Admin,
+  ErrorPage,
+  Splash,
+};
