40cf4891e5a7140375689126389c529d2bcaade9
mradul kumar katiyar <karelaking277@gmai.com>
2024-09-04 23:40:17 +0530

    feat: Add new dependencies and navigation bar component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,12 @@
 import BorderdRactangleButton from '../components/buttons/BorderdRactangleButton';
+import NavigationBar from '../components/navigation/NavigationBar';
 const App = () => {
     return (
         <>
-            <div class="h-screen overflow-y-auto">
-                <header class="sticky top-0 bg-blue-200 p-4">Header</header>
-                <main class="h-full flex-1 bg-blue-50 p-4">Content</main>
-                <footer class="flex justify-center items-center bg-blue-200 p-4 text-white">Footer</footer>
+            <div className="h-screen overflow-y-auto">
+                <NavigationBar/>
+                <main className="h-full flex-1 bg-blue-50 p-4">Content</main>
+                <footer className="flex justify-center items-center bg-blue-200 p-4 text-white">Footer</footer>
             </div>
         </>
     );
