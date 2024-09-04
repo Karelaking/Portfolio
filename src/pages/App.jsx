02f151d99142b79edf75bc74ad553430b043c668
mradul kumar katiyar <karelaking277@gmai.com>
2024-09-04 15:00:18 +0530

    feat: Add new layout with header, main, and footer sections

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,12 @@
+import BorderdRactangleButton from '../components/buttons/BorderdRactangleButton';
 const App = () => {
     return (
         <>
-            <h1>hello world</h1>
+            <div class="h-screen overflow-y-auto">
+                <header class="sticky top-0 bg-blue-200 p-4">Header</header>
+                <main class="h-full flex-1 bg-blue-50 p-4">Content</main>
+                <footer class="flex justify-center items-center bg-blue-200 p-4 text-white">Footer</footer>
+            </div>
         </>
     );
 };
