79c028e07b36144385fc5d42b90380fc85aa93b2
Karela King <karelaking277@gmail.com>
2025-11-04 19:40:43 +0530

    bug fix

--- app/cosmos/[fixture]/page.tsx
+++
@@ -1,6 +0,0 @@
-import * as cosmosImports from "../../../cosmos.imports";
-import { nextCosmosPage, nextCosmosStaticParams } from "react-cosmos-next";
-
-export const generateStaticParams = nextCosmosStaticParams(cosmosImports);
-
-export default nextCosmosPage(cosmosImports);
