c00dddf4984205bd621b7990996634d72485c8c7
Karela King <karelaking277@gmail.com>
2025-12-02 09:11:48 +0530

    feat(SkillsSection): add SkillsSection component for skills display refactor(AboutSection): remove unused skillsRef and clean up code

---
+++ components/skills-section.tsx
@@ -0,0 +1,16 @@
+
+
+import PageLayout from '@/app/layout/page'
+import React from 'react'
+import Heading from './heading';
+
+const SkillsSection = () => {
+    
+  return (
+    <PageLayout sectionId="skills" className="bg-white dark:bg-slate-800">
+     <Heading>My Skills</Heading>
+    </PageLayout>
+  )
+}
+
+export default SkillsSection
