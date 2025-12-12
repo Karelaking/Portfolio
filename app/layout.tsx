319315c2c7d98795c10155639da146bb291638fe
Karela King <karelaking277@gmail.com>
2025-12-13 01:16:00 +0530

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import "./globals.css";
 import { gsap } from "gsap";
 import type { Metadata } from "next";
 import { useGSAP } from "@gsap/react";
-import Header from "./layout/header";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Provider } from "./provider/provider";
 import Footer from "./layout/footer";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { Header, NavItems } from "./layout/header";
 
 gsap.registerPlugin(useGSAP);
 
@@ -24,6 +25,26 @@ export const metadata: Metadata = {
   description: "this is the mradul kumar's portfolio website.",
 };
 
+const navItems: NavItems[] = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    icon: (
+      <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,8 +56,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>
-          {/* <Header /> */}
-          <div className="min-w-full min-h-screen">{children}</div>
+          <div className="relative w-full h-full min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
+            <Header navItems={navItems} />
+             {children}
+          </div>
           <Footer />
         </Provider>
       </body>
--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,22 +1,83 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "motion/react";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { caveat } from "@/fonts/fonts";
 import ThemeModeToggleButton from "@/components/theme-toggle-button";
 
 
-const Header = (): React.JSX.Element => {
+export interface NavItems {
+  name: string;
+  link: string;
+  icon?: React.JSX.Element;
+}
+
+export const Header = ({
+  navItems,
+  className,
+}: {
+  navItems: NavItems[];
+  className?: string;
+}) => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+
+      if (scrollYProgress.get() < 0.05) {
+        setVisible(false);
+      } else {
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    }
+  });
+
   return (
-    <header className="bg-white dark:bg-slate-800 border-b fixed w-full top-0 z-50">
-      <div className="w-full mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-18 shadow-lg border-0 border-transparent">
-        <Link href="/" className={`text-3xl font-bold ${caveat.className}`}>
-          MRADUL
-        </Link>
-        <div className="flex lg:gap-x-12 itecms-center justify-center">
-          <ThemeModeToggleButton />
-        </div>
-      </div>
-    </header>
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "flex max-w-fit fixed bottom-10 inset-x-0 mx-auto border border-transparent dark:border-white/20 rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-5000 pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          className
+        )}
+      >
+        {navItems.map((navItem: NavItems, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative dark:text-white items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </Link>
+        ))}
+        <ThemeModeToggleButton />
+      </motion.div>
+    </AnimatePresence>
   );
 };
-
-export default Header;
