dfc8107667fd6df6988cc8fe50fa046eebde7515
Karela King <karelaking277@gmail.com>
2025-12-16 01:14:07 +0530

    feat: Introduce Hero, Skills, and Expertise sections, update core dependencies, and configure Next.js dev tools.

--- components/expertise-section.tsx
+++ components/expertise-section.tsx
@@ -87,7 +87,7 @@ const Feature = ({
           {title}
         </span>
       </div>
-      <p className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
+      <div className="text-sm text-neutral-600 dark:text-neutral-300 max-w-xs relative z-10 px-10">
         {description.map((desc, index) => (
           <span key={index}>
             <div className="text-md inline text-center">
@@ -98,7 +98,7 @@ const Feature = ({
             </div>
           </span>
         ))}
-      </p>
+      </div>
     </div>
   );
 };
