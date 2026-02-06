546c8751a13a21cf066686e5e3c26af653f93f68
Karela King <karelaking277@gmail.com>
2026-02-06 22:45:43 +0530

    deleted all the files

--- types/skills.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IconProps, Icon } from "@tabler/icons-react";
-import { ForwardRefExoticComponent, RefAttributes } from "react";
-
-export interface Skill {
-  name: string;
-  icon: string;
-  discription: string;
-}
